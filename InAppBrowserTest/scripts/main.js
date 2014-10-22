814f15b582b949befce4959fe088098ea55c00fe
Siyka Marinova <siyka.marinova@gmail.com>
2014-10-22 08:43:38 +0000

    Changes made by Siyka Marinova (siyka.marinova@gmail.com).

---
+++ InAppBrowserTest/scripts/main.js
@@ -0,0 +1,52 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    navigator.splashscreen.hide();
+    alert(document.URL);
+    var app = new App();
+    app.run();
+}
+
+function App() {
+}
+
+App.prototype = {
+    resultsField: null,
+     
+    run: function() {
+        var that = this,
+        scanButton = document.getElementById("scanButton");
+        
+        that.resultsField = document.getElementById("result");
+        
+        scanButton.addEventListener("click",
+                                    function() { 
+                                        that._scan.call(that); 
+                                    });
+    },
+    
+    _scan: function() {
+        var that = this;
+        if (window.navigator.simulator === true) {
+            alert("Not Supported in Simulator.");
+        }
+        else {
+            cordova.plugins.barcodeScanner.scan(
+                function(result) {
+                    if (!result.cancelled) {
+                        that._addMessageToLog(result.format + " | " + result.text);    
+                    }
+                }, 
+                function(error) {
+                    console.log("Scanning failed: " + error);
+                });
+        }
+    },
+
+    _addMessageToLog: function(message) {
+        var that = this,
+        currentMessage = that.resultsField.innerHTML;
+        
+        that.resultsField.innerHTML = currentMessage + message + '<br />'; 
+    }
+}
